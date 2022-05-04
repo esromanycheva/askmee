15492b4d599ed94b8e7bdab842f506fcf87ab389
esromanycheva <esromanycheva@gmail.com>
2022-05-04 21:45:23 +0300

    Initial commit and solved lesson

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,51 @@
+class QuestionsController < ApplicationController
+  before_action :set_question, only: %i[update show destroy edit hide]
+
+  def create
+    question = Question.create(question_params)
+
+    redirect_to question_path(question), notice: 'Новый вопрос создан!'
+  end
+
+  def update
+    @question.update(question_params)
+
+    redirect_to question_path(@question), notice: 'Сохранили вопрос!'
+  end
+
+  def destroy
+    @question.destroy
+
+    redirect_to questions_path, notice: 'Вопрос удален'
+  end
+
+  def show
+  end
+
+  def index
+    @question = Question.new
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def edit
+  end
+
+  def hide
+    @question.update(hidden: true)
+    redirect_to questions_path, notice: 'Вопрос скрыт'
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:body, :user_id, :hidden)
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
+end
