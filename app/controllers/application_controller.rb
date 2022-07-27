80b0497a8c10f6c625ebb955746cc09d30cba09f
esromanycheva <94757683+esromanycheva@users.noreply.github.com>
2022-07-27 17:01:42 +0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,12 +10,4 @@ class ApplicationController < ActionController::Base
   def redirect_with_alert
     redirect_to root_path, alert: 'Вам сюда нельзя'
   end
-
-  def render_404
-    respond_to do |format|
-      format.html { render :file => "#{Rails.root}/public/404.html", layout: false, status: :not_found }
-      format.xml  { head :not_found }
-      format.any  { head :not_found }
-    end
-  end
 end
