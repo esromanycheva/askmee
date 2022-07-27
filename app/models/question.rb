90386a1453d4bc700af6b305f7000f44f1169745
esromanycheva <94757683+esromanycheva@users.noreply.github.com>
2022-07-27 17:02:22 +0300

    Update question.rb

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,13 +6,22 @@ class Question < ApplicationRecord
   has_many :hashtags, through: :question_hashtags
 
   validates :body, presence: true, length: { maximum: 280 }
-  after_commit :add_hashtags
+
+  after_save_commit :add_hashtags
+  after_destroy_commit :clean_up_tags
 
   private
 
   def add_hashtags
-    matches = "#{body} #{answer}".downcase.scan(Hashtag::VALID_HASHTAG_REGEX)
-    tags = matches.map { |m| Hashtag.find_or_create_by!(name: m.gsub('#', '')) }
-    self.hashtags = tags.uniq
+    question_hashtags.destroy_all
+    matched_tags = "#{body} #{answer}".downcase.scan(Hashtag::VALID_HASHTAG_REGEX)
+
+    matched_tags.each do |matched_tag|
+      self.hashtags << Hashtag.find_or_create_by!(name: matched_tag.gsub('#', ''))
+    end
+  end
+
+  def clean_up_tags
+    Hashtag.unused.destroy_all
   end
 end
