28cb98a9e14f863ccf96f89fd3840821a3062851
esromanycheva <94757683+esromanycheva@users.noreply.github.com>
2022-07-27 17:02:04 +0300

    Update hashtag.rb

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -3,4 +3,6 @@ class Hashtag < ApplicationRecord
 
   has_many :question_hashtags, dependent: :destroy
   has_many :questions, through: :question_hashtags
+
+  scope :unused, -> { includes(:question_hashtags).where(question_hashtags: { question_id: nil}) } 
 end
